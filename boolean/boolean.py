c7bd72d4c12ac8053121f1562fc6b89c5ecd31b5
Qi-rui Chen <Qi-rui.Chen@hotmail.co.uk>
2015-03-07 14:42:02 +0000

    Update boolean.py

--- boolean/boolean.py
+++ boolean/boolean.py
@@ -3,7 +3,8 @@ Boolean Algebra.
 
 This module defines a Boolean Algebra over the set {TRUE, FALSE} with boolean
 variables and the boolean functions AND, OR, NOT. For extensive documentation
-look either into the docs directory or view it online.
+look either into the docs directory or view it online, at
+https://booleanpy.readthedocs.org/en/latest/.
 
 Copyright (c) 2009-2010 Sebastian Kraemer, basti.kr@gmail.com
 Released under revised BSD license.
