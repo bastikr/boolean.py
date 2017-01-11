b1206e887dc6eb31645ed581e05d948b076a1aab
Sebastian Kraemer <basti.kr@gmail.com>
2017-01-11 19:20:23 +0100

    Switch from pngmath to mathjax do display formulas in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.append(os.path.abspath('../boolean/'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.pngmath', 'sphinx.ext.inheritance_diagram']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.mathjax', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
