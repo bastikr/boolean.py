00200eadf5a8495ef769cf57c05191de47de0fce
Alexander Lisianoi <all3fox@gmail.com>
2017-06-17 13:12:06 +0200

    Move AdvancedAlgebra into separate mock module
    Signed-off-by: Aleksandr Lisianoi <all3fox@gmail.com>

--- tests/test_boolean.py
+++ tests/test_boolean.py
@@ -13,11 +13,6 @@ from __future__ import print_function
 
 from boolean.boolean import PARSE_UNKNOWN_TOKEN
 
-try:
-    basestring  # Python 2
-except NameError:
-    basestring = str  # Python 3
-
 import unittest
 from unittest.case import expectedFailure
 
@@ -36,6 +31,8 @@ from boolean.boolean import PARSE_INVALID_SYMBOL_SEQUENCE
 from boolean.boolean import PARSE_INVALID_EXPRESSION
 from boolean.boolean import PARSE_INVALID_NESTING
 
+from tests.mock_advanced_algebra import AdvancedAlgebra
+from tests.mock_advanced_algebra import PlainVar, ColonDotVar
 
 class BooleanAlgebraTestCase(unittest.TestCase):
 
@@ -146,112 +143,6 @@ class BooleanAlgebraTestCase(unittest.TestCase):
         self.assertEqual(expected, expr)
 
     def test_parse_with_advanced_tokenizer_example(self):
-        import tokenize
-
-        try:
-            from io import StringIO
-        except ImportError:
-            try:
-                from cStringIO import StringIO
-            except ImportError:
-                from StringIO import StringIO
-
-
-        class PlainVar(Symbol):
-            "Plain boolean variable"
-
-        class ColonDotVar(Symbol):
-            "Colon and dot-separated string boolean variable"
-
-        class AdvancedAlgebra(BooleanAlgebra):
-            def tokenize(self, expr):
-                """
-                Example custom tokenizer derived from the standard Python tokenizer
-                with a few extra features: #-style comments are supported and a
-                colon- and dot-separated string is recognized and stored in custom
-                symbols. In contrast with the standard tokenizer, only these
-                boolean operators are recognized : & | ! and or not.
-
-                For more advanced tokenization you could also consider forking the
-                `tokenize` standard library module.
-                """
-
-                if not isinstance(expr, basestring):
-                    raise TypeError('expr must be string but it is %s.' % type(expr))
-
-                # mapping of lowercase token strings to a token object instance for
-                # standard operators, parens and common true or false symbols
-                TOKENS = {
-                    '&': TOKEN_AND,
-                    'and': TOKEN_AND,
-                    '|': TOKEN_OR,
-                    'or': TOKEN_OR,
-                    '!': TOKEN_NOT,
-                    'not': TOKEN_NOT,
-                    '(': TOKEN_LPAR,
-                    ')': TOKEN_RPAR,
-                    'true': TOKEN_TRUE,
-                    '1': TOKEN_TRUE,
-                    'false': TOKEN_FALSE,
-                    '0': TOKEN_FALSE,
-                    'none': TOKEN_FALSE,
-                }
-
-                ignored_token_types = (
-                    tokenize.NL, tokenize.NEWLINE, tokenize.COMMENT,
-                    tokenize.INDENT, tokenize.DEDENT,
-                    tokenize.ENDMARKER
-                )
-
-                # note: an unbalanced expression may raise a TokenError here.
-                tokens = ((toktype, tok, row, col,) for toktype, tok, (row, col,), _, _
-                          in tokenize.generate_tokens(StringIO(expr).readline)
-                          if tok and tok.strip())
-
-                COLON_DOT = (':', '.',)
-
-                def build_symbol(current_dotted):
-                    if current_dotted:
-                        if any(s in current_dotted for s in COLON_DOT):
-                            sym = ColonDotVar(current_dotted)
-                        else:
-                            sym = PlainVar(current_dotted)
-                        return sym
-
-                # accumulator for dotted symbols that span several `tokenize` tokens
-                dotted, srow, scol = '', None, None
-
-                for toktype, tok, row, col in tokens:
-                    if toktype in ignored_token_types:
-                        # we reached a break point and should yield the current dotted
-                        symbol = build_symbol(dotted)
-                        if symbol is not None:
-                            yield symbol, dotted, (srow, scol)
-                            dotted, srow, scol = '', None, None
-
-                        continue
-
-                    std_token = TOKENS.get(tok.lower())
-                    if std_token is not None:
-                        # we reached a break point and should yield the current dotted
-                        symbol = build_symbol(dotted)
-                        if symbol is not None:
-                            yield symbol, dotted, (srow, scol)
-                            dotted, srow, scol = '', 0, 0
-
-                        yield std_token, tok, (row, col)
-
-                        continue
-
-                    if toktype == tokenize.NAME or (toktype == tokenize.OP and tok in COLON_DOT):
-                        if not dotted:
-                            srow = row
-                            scol = col
-                        dotted += tok
-
-                    else:
-                        raise TypeError('Unknown token: %(tok)r at line: %(row)r, column: %(col)r' % locals())
-
         test_expr = '''
             (colon1:dot1.dot2 or colon2_name:col_on3:do_t1.do_t2.do_t3 )
             and
