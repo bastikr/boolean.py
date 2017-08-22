a8ed756d0eae7554c9dae818f4f2de0b36627175
Alexander Lisianoi <all3fox@gmail.com>
2017-08-22 13:29:26 +0200

    Add tests for BaseElement
    Signed-off-by: Aleksandr Lisianoi <all3fox@gmail.com>

--- tests.js/test_boolean.js
+++ tests.js/test_boolean.js
@@ -4,14 +4,64 @@ let boolean = require('../boolean.js/__javascript__/boolean.js')
 
 let BooleanAlgebra = boolean.boolean.BooleanAlgebra
 
-describe('BooleanAlgebra', function() {
-    it('TRUE and FALSE make sense', function() {
-        let algebra = BooleanAlgebra()
+describe('BaseElement', function() {
+    let algebra
 
+    beforeEach(function() {
+        algebra = BooleanAlgebra()
+    })
+
+    it('TRUE and FALSE make sense', function() {
         assert.equal(algebra.TRUE, algebra.TRUE)
         assert.equal(algebra.FALSE, algebra.FALSE)
 
         assert.ok(algebra.TRUE === algebra.TRUE)
         assert.ok(algebra.FALSE === algebra.FALSE)
+
+        assert.notEqual(algebra.TRUE, algebra.FALSE)
+        assert.notEqual(algebra.FALSE, algebra.TRUE)
+
+        assert.ok(algebra.TRUE != algebra.FALSE)
+        assert.ok(algebra.FALSE != algebra.TRUE)
+    })
+
+    it('literals are empty sets', function() {
+        // algebra.TRUE.literals and algebra.FALSE.literals are weird objects
+        assert.ok(algebra.TRUE .literals().__class__.__name__ === 'set')
+        assert.ok(algebra.FALSE.literals().__class__.__name__ === 'set')
+    })
+
+    it('.literalize() works on basics', function() {
+        assert.equal(algebra.TRUE.literalize(), algebra.TRUE)
+        assert.equal(algebra.FALSE.literalize(), algebra.FALSE)
+
+        assert.notEqual(algebra.TRUE.literalize(), algebra.FALSE)
+        assert.notEqual(algebra.FALSE.literalize(), algebra.TRUE)
+    })
+
+    it('.simplify() works on basics', function() {
+        assert.equal(algebra.TRUE.simplify(), algebra.TRUE)
+        assert.equal(algebra.FALSE.simplify(), algebra.FALSE)
+
+        assert.notEqual(algebra.TRUE.simplify(), algebra.FALSE)
+        assert.notEqual(algebra.FALSE.simplify(), algebra.TRUE)
+    })
+
+    it('dual works on basics', function() {
+        assert.equal(algebra.TRUE.dual, algebra.FALSE)
+        assert.equal(algebra.FALSE.dual, algebra.TRUE)
+    })
+
+    it('order works on basics', function() {
+        assert.ok(algebra.FALSE < algebra.TRUE)
+        assert.ok(algebra.TRUE > algebra.FALSE)
+    })
+
+    it('converting to string makes sense', function() {
+        assert.ok(algebra.FALSE.toString() === '0')
+        assert.ok(algebra.TRUE .toString() === '1')
+
+        assert.ok(algebra.FALSE.__repr__() === 'FALSE')
+        assert.ok(algebra.TRUE .__repr__() === 'TRUE' )
     })
 })
